e726251f486554ed8688b8941c7a5bda2752ccdc
s tech <email@sarcas.tech>
2016-10-22 22:37:50 -0400

    Refactoring to have module return a readable stream. Updated README

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ipfreely
 Get the IP address of your favorite domains. Freely.
+
+## Usage
+`ipfreely` is a function that takes an argument of an array containing domain names.
+This function returns a readable stream.
+
+```javascript
+// calling ipfreely will return a readable stream
+let ipfreely = require('ipfreely')
+
+// create a writeable stream to save ipfreely's output to a file
+let fs = require('fs')
+let writeStream = fs.createWriteStream('iplist.txt')
+
+// call and pass an array of domain names
+// readable stream will send a line of text for each domain/ip combination
+ipfreely(['google.com', 'github.com', 'js.foundation'])
+  .pipe(writeStream)
+```
--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 'use strict'
 
 let exec = require('child_process').exec
-let fs = require('fs')
 let stream = require('stream')
 let errorHandler = require('./lib/error-handler')
-let dataStream = new stream.Readable()
-dataStream._read = function () {}
-dataStream.on('error', errorHandler)
 
-let fileWriter = fs.createWriteStream('ips.txt')
-fileWriter.on('error', errorHandler)
+function ipfreely (siteList) {
+  let readStream = new stream.Readable()
+  readStream._read = function () {} // no op
 
-dataStream.pipe(fileWriter)
-
-let ipfreely = function (sites) {
-  sites.forEach((site) => {
+  siteList.forEach((site) => {
     exec(`dig +short ${site}`, (err, output) => {
       if (err) {
         errorHandler(err)
         return false
       }
 
-      dataStream.push(`${site}: ${output.match(/^[0-9\.]+/)[0]}\n`)
+      readStream.push(`${site}: ${output.match(/^[0-9\.]+/)[0]}\n`)
     })
   })
+
+  return readStream
 }
 
 module.exports = ipfreely
