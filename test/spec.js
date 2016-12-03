5e9403537acc9cdff64dd18239f58adc816fa748
s tech <email@sarcas.tech>
2016-12-03 11:04:16 -0500

    Break out nested test. Move spec file to /test directory. Update package.json test script.

--- test/spec.js
+++ test/spec.js
@@ -2,21 +2,23 @@
 
 const test = require('ava')
 const stream = require('stream')
-const ipfreely = require('./index')
+const ipfreely = require('../index')
 const domainList = ['github.com']
+const chunkRegex = /^[a-z\.]+:[0-9\.]+\n/gi
+const foo = ipfreely(domainList)
 
 test('ipfreely should be a function', function (t) {
   t.is(typeof ipfreely, 'function')
 })
 
-test.cb('ipfreely should return a readable stream', function (t) {
-  let foo = ipfreely(domainList)
+test(' calling ipfreely should return a readable stream', function (t) {
   t.is(foo instanceof stream.Readable, true)
+})
 
+test.cb('readable stream should create a colon-dilimited string', function (t) {
   foo.setEncoding('utf8')
   foo.on('data', function (chunk) {
-    t.is(typeof chunk, 'string')
-    t.is(chunk.indexOf('github.com:'), 0)
+    t.is(chunkRegex.test(chunk), true)
     t.end()
   })
 })
