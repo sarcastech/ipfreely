477cfddd972c8250ab681940dc924e0ec976f7e9
s tech <email@sarcas.tech>
2016-12-09 16:54:45 -0500

    replace child_process.exec() with dns.resolve()

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let exec = require('child_process').exec
+let resolve = require('dns').resolve
 let stream = require('stream')
 
 function ipfreely (siteList) {
@@ -8,12 +8,11 @@ function ipfreely (siteList) {
   readStream._read = function () {} // no op
 
   siteList.forEach((site) => {
-    exec(`dig +short ${site}`, (err, output) => {
+    resolve(site, (err, ipList) => {
       if (err) {
         readStream.emit('error', err)
       }
-
-      readStream.push(`${site}:${output.match(/^[0-9\.]+/)[0]}\n`)
+      readStream.push(`${site}:${ipList[0]}\n`)
     })
   })
 
